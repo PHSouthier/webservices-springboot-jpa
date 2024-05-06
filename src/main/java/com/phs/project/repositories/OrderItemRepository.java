deb923216e5653e3a3472774766212ad1a764805
Pedro Southier <phsouthier@hotmail.com>
2024-05-05 21:43:29 -0300

    Modified Java class creation for OrderItemRepository

--- src/main/java/com/phs/project/repositories/OrderItemRepository.java
+++ src/main/java/com/phs/project/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.phs.project.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.phs.project.entities.OrderItem;
+import com.phs.project.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
 
 }
